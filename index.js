8df0286a7a0a26807aa810f1b99c4dce41e4af60
gouthammanigonda <gouthammanigonda737@gamil.com>
2022-05-11 12:09:29 +0000

    Error:drive not found

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const dbPath = path.join(__dirname, "goodreads.db");
+let db = null;
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      drive: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log(`server is running at http://localhost:3000/`);
+    });
+  } catch (e) {
+    console.log(`DB Error ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
